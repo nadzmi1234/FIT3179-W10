7835651654ca0706ccf3e9a77d2d52feaf2c401a
nadzmi1234 <111639512+nadzmi1234@users.noreply.github.com>
2022-10-10 23:40:13 +1100

    Add files via upload

---
+++ W10_Homework/charts.js
@@ -0,0 +1,20 @@
+// First Chart
+var vg_1 = "chart1_vg.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+    
+}).catch(console.error);
+
+// Second/Third Chart
+var vg_23 = "chart23_vg.json";
+
+vegaEmbed("#chart23", vg_23).then(function(result) {
+    
+}).catch(console.error);
+
+// Fourth/Fifth Chart
+var vg_45 = "chart45_vg.json";
+
+vegaEmbed("#chart45", vg_45).then(function(result) {
+    
+}).catch(console.error);
